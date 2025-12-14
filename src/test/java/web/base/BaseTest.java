4fec9683c1455e056da96af56c7410277f5e074f
DaNN <idaniil535@gmail.com>
2025-12-14 16:39:36 +0300

    upd

--- src/test/java/web/base/BaseTest.java
+++ src/test/java/web/base/BaseTest.java
@@ -11,8 +11,7 @@ import org.junit.jupiter.api.BeforeAll;
 public abstract class BaseTest {
 
     private static void setup() {
-        WebDriverManager.edgedriver().setup();
-        Configuration.browser = "edge";
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.headless = false;
         Configuration.timeout = 20000;
