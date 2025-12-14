060afda31d42f9a6a91c387210d05ed6ce4f8d94
DaNN <idaniil535@gmail.com>
2025-12-14 16:49:47 +0300

    upd

--- src/test/java/web/base/BaseTest.java
+++ src/test/java/web/base/BaseTest.java
@@ -7,14 +7,21 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public abstract class BaseTest {
 
     private static void setup() {
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--start-maximized");
+        Configuration.browserCapabilities = options;
+
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.timeout = 20000;
         Configuration.pageLoadStrategy = "none";
         Configuration.pageLoadTimeout = 0;
