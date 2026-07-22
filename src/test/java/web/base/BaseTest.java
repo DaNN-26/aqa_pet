b8619530a67555a8dc00304c4b9c7f532125bce7
DaNN <idaniil535@gmail.com>
2026-07-22 16:16:29 +0300

    upd

--- src/test/java/web/base/BaseTest.java
+++ src/test/java/web/base/BaseTest.java
@@ -3,7 +3,6 @@ package web.base;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,16 +11,17 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public abstract class BaseTest {
 
     private static void setup() {
-        Configuration.remote = "http://selenoid:4444/wd/hub";
+        Configuration.remote = "http://host.docker.internal:4444/wd/hub";
         Configuration.browser = "chrome";
         Configuration.headless = false;
-        Configuration.browserSize = "1920x1080"; // рекомендуем установить размер
+        Configuration.browserSize = "1920x1080";
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
+        options.addArguments("--remote-allow-origins=*");
 
         Configuration.browserCapabilities = options;
 
